8e7ab484c0d9d898649827de0eb6abaa14eff01e
DaGeRe <davidgeorg_reichelt@dagere.de>
2023-03-28 21:40:27 +0200

    Move more classes from peass

---
+++ src/main/java/de/dagere/peass/config/FolderConfig.java
@@ -0,0 +1,12 @@
+package de.dagere.peass.config;
+
+import java.util.List;
+
+public interface FolderConfig {
+
+   List<String> getClazzFolders();
+
+   List<String> getTestClazzFolders();
+
+   List<String> getAllClazzFolders();
+}
